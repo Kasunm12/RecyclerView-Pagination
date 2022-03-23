8201718cbdc3e02e78b9d92146fb248567fbfdff
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2022-03-23 17:50:51 +0530

    add to url,retrofit funtions

--- app/src/main/java/com/edulifesolutions/recyclerviewpaginationapplication/MainActivity.java
+++ app/src/main/java/com/edulifesolutions/recyclerviewpaginationapplication/MainActivity.java
@@ -1,14 +1,61 @@
 package com.edulifesolutions.recyclerviewpaginationapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Initialize variable
+    NestedScrollView nestedScrollView;
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+
+    ArrayList<MainData> dataArrayList = new ArrayList<>();
+    MainAdapter adapter;
+
+    int page = 1, limit = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assign variable
+        nestedScrollView = findViewById(R.id.scroll_view);
+        recyclerView = findViewById(R.id.recycler_view);
+        progressBar = findViewById(R.id.progress_bar);
+
+        //Initialize adapter
+        adapter = new MainAdapter(MainActivity.this, dataArrayList);
+
+        //Set layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //set adapter
+        recyclerView.setAdapter(adapter);
+
+        //create get data method
+        getData(page, limit);
+
     }
+
+    private void getData(int page, int limit) {
+
+        //Initialize retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://picsum.photos/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+    }
+
 }
